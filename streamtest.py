9b0fb9bfd2a89b4395b42ac1e05ce59947d5b5db
ObjectionTheory <therandomguy265@gmail.com>
2022-02-26 12:04:14 +0000

    records 20

--- streamtest.py
+++ streamtest.py
@@ -24,7 +24,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
 
         conn, addr = sock.accept()
         stream = conn.makefile("wb")
-        picam.encoder.output = stream
+        picam2.encoder.output = stream
+        time.sleep(20)
+        picam2.stop()
+        picam2.stop_encoder()
+        conn.close()
+        
     except KeyboardInterrupt:
         sock.close()
         exit()
