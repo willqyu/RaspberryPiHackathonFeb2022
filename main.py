18bd8c39954a088f9610a122fe5f4e612737216d
ObjectionTheory <therandomguy265@gmail.com>
2022-02-26 13:11:03 +0000

    sock

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from curses import raw
+import socket
 from picamera2 import *
 from null_preview import *
 
 cam = Picamera2()
-cam.configure(cam.still_configuration())
+config = cam.still_configuration()
+cam.configure(config)
+
 preview = NullPreview(cam)
 
-cam.start()
-raw_np_array = cam.capture_array("raw")
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(("", 42069))
+    sock.listen()
 
-print(raw_np_array)
+    conn, addr = sock.accept()
+
+    cam.start()
+
+    stream = conn.makefile("wb")
+    np_array = cam.capture_array()
+    stream.write(np_array)
+    cam.stop()
+    conn.close()
