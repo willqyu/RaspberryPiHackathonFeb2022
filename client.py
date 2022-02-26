d2d4dcc698e0d21acbab64139a4d64fc4b99d353
ObjectionTheory <therandomguy265@gmail.com>
2022-02-26 15:58:40 +0000

    client refactor

--- client.py
+++ client.py
@@ -6,54 +6,22 @@ import cv2
 from joblib import load
 from mediapipe.python.solutions import drawing_utils, hands
 
+from data_handling import load_model, process_hand
+
 # load the models
 gesture_model = load('./model/gesture_model.pkl')
 hand_model = hands.Hands(static_image_mode=True, 
     min_detection_confidence=0.7, 
     min_tracking_confidence=0.7, max_num_hands=1)
-txt_offset = (-60, 30)
 
 HOST = '192.168.137.71'    # The remote host
 PORT = 42069              # The same port as used by the server
 
-def process_hand(image):
-    # pass video into network
-    resolution = (640, 480)
-    results = hand_model.process(image)
-    gesture, confidence = 0, 0
-    # process results
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            x, y = [], []
-            for lm in hand_landmarks.landmark:
-                x.append(lm.x)
-                y.append(lm.y)
-                
-            txt_pos = np.add(np.multiply(resolution, (x[0], y[0])), txt_offset)
-                
-            # normalize points
-            points = np.asarray([x,y])
-            min = points.min(axis=1, keepdims=True)
-            max = points.max(axis=1, keepdims=True)
-            normalized = np.stack((points-min)/(max-min), axis=1).flatten()
-
-            # get prediction and confidence
-            pred = gesture_model.predict_proba([normalized])
-            gesture = pred.argmax(axis=1)[0]
-            confidence = pred.max()
-
-            # add text
-            cv2.putText(image, "'{}',{:.1%}".format(gesture, confidence), 
-            txt_pos.astype(int), cv2.FONT_HERSHEY_DUPLEX,  1, 
-            (0, 255, 255), 1, cv2.LINE_AA)
-
-    return gesture, confidence
             
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     
-    frame_count = 0
     incoming = b''
     try:
         while True:
@@ -66,18 +34,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 incoming += message[4:]
             elif message[-4:] == b"tail":
                 incoming += message[:-4]
-                frame_count += 1
-                #print("frame", frame_count)
+                
                 decoded_image = np.frombuffer(incoming, dtype = 'uint8').reshape((480, 640, 3))
                 rgb_image = cv2.cvtColor(decoded_image, cv2.COLOR_BGR2RGB)
                 cv2.imshow("Camera", rgb_image)
-                cv2.waitKey(3)
-                gesture, confidence = process_hand(decoded_image)
+                cv2.waitKey(1)
+                gesture, confidence = process_hand(decoded_image, hand_model)
                 if confidence:
                     print(gesture)
-                #time.sleep(0.01)
-                return_message = "g" + str(gesture)
-                s.send(return_message.encode())
+                    return_message = "g" + str(gesture)
+                    s.send(return_message.encode())
+                
             else:
                 incoming+=message
             
--- data_handling.py
+++ data_handling.py
@@ -2,44 +2,36 @@ from gesture_recognition import *
 import cv2
 import time
 import numpy as np
-from joblib import load
-from mediapipe.python.solutions import drawing_utils, hands
+    
+def process_hand(image, hand_model):
+    # pass video into network
+    resolution = (640, 480)
+    results = hand_model.process(image)
+    gesture, confidence = 0, 0
+    # process results
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            x, y = [], []
+            for lm in hand_landmarks.landmark:
+                x.append(lm.x)
+                y.append(lm.y)
+                
+            txt_pos = np.add(np.multiply(resolution, (x[0], y[0])), (-60, 30))
+                
+            # normalize points
+            points = np.asarray([x,y])
+            min = points.min(axis=1, keepdims=True)
+            max = points.max(axis=1, keepdims=True)
+            normalized = np.stack((points-min)/(max-min), axis=1).flatten()
 
-# load the models
-gesture_model = load('./model/gesture_model.pkl')
-hand_model = hands.Hands(static_image_mode=True, 
-    min_detection_confidence=0.7, 
-    min_tracking_confidence=0.7, max_num_hands=1)
-txt_offset = (-60, 30)
+            # get prediction and confidence
+            pred = gesture_model.predict_proba([normalized])
+            gesture = pred.argmax(axis=1)[0]
+            confidence = pred.max()
 
+            # add text
+            cv2.putText(image, "'{}',{:.1%}".format(gesture, confidence), 
+            txt_pos.astype(int), cv2.FONT_HERSHEY_DUPLEX,  1, 
+            (0, 255, 255), 1, cv2.LINE_AA)
 
-# pass video into network
-image = []
-resolution = (640, 480)
-results = hand_model.process(image)
-
-# process results
-if results.multi_hand_landmarks:
-    for hand_landmarks in results.multi_hand_landmarks:
-        x, y = [], []
-        for lm in hand_landmarks.landmark:
-            x.append(lm.x)
-            y.append(lm.y)
-            
-        txt_pos = np.add(np.multiply(resolution, (x[0], y[0])), txt_offset)
-            
-        # normalize points
-        points = np.asarray([x,y])
-        min = points.min(axis=1, keepdims=True)
-        max = points.max(axis=1, keepdims=True)
-        normalized = np.stack((points-min)/(max-min), axis=1).flatten()
-
-        # get prediction and confidence
-        pred = gesture_model.predict_proba([normalized])
-        gesture = pred.argmax(axis=1)[0]
-        confidence = pred.max()
-
-        # add text
-        cv2.putText(image, "'{}',{:.1%}".format(gesture, confidence), 
-        txt_pos.astype(int), cv2.FONT_HERSHEY_DUPLEX,  1, 
-        (0, 255, 255), 1, cv2.LINE_AA)
+    return gesture, confidence, image
