9d0ae3b84499c27f5d5d5bc3064a429c330019b2
ObjectionTheory <therandomguy265@gmail.com>
2022-02-26 12:55:59 +0000

    started camera

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from curses import raw
 from picamera2 import *
 
 cam = Picamera2()
-
+cam.configure(cam.still_configuration)
+cam.start()
 raw_np_array = cam.capture_array("raw")
 
 print(raw_np_array)
