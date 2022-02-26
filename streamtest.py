54255f265ff14a14542c0bca2fee0bf67c811827
callumstephenson <callumstephenson03@gmail.com>
2022-02-26 11:49:27 +0000

    added streamtest.py

---
+++ streamtest.py
@@ -0,0 +1,30 @@
+from picamera2 import *
+from h264_encoder import *
+from null_preview import *
+import socket
+import os
+import time
+import keyboard 
+
+# init camera
+picam = Picamera2()
+video_config = picam.video_configuration({"size": (1280, 720)})
+picam.configure(video_config)
+preview = NullPreview(picam)
+encoder = H264Encoder(1000000)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(("0.0.0.0", 10001))
+    sock.listen()
+
+    picam.encoder = encoder
+    picam.start_encoder()
+    picam.start()
+
+    conn, addr = sock.accept()
+    stream = conn.makefile("wb")
+    picam.encoder.output = stream
+
+if keyboard.is_pressed("q"):
+    exit()
