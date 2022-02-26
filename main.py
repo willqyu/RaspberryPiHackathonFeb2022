b21f10ddbf51ad4855ef05b5b633088421ca607d
ObjectionTheory <therandomguy265@gmail.com>
2022-02-26 14:44:23 +0000

    shape

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ cam.start()
 np_array = cam.capture_array()
 np_bytes = np_array.tobytes()
 print(len(np_bytes))
-print(np_bytes.shape)
+print(np_array.shape)
 
 conn.send(b"head")
 conn.send(np_bytes)
